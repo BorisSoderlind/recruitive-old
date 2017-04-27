d304de211b1e5f0028d81a3e3d5bd71c2190b986
edrlgo <emi.lop96@gmail.com>
2017-04-27 15:58:44 +0200

    Knapparna längst ner - prototyp

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
-<?php wp_footer();?>
+<?php 
+/*wp_footer();*/?>
+</div>
+
+<div class="btn-group btn-block fixed" role="group">
+	<a href="#" type="button" class="btn btn-jobb">Lediga jobb</a>
+	<a href="#" type="button" class="btn btn-cv">Sök jobb utan CV</a>
+	<a href="#" type="button" class="btn btn-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+	<a href="#" type="button" class="btn btn-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+</div>
+
 </body>
 </html>
