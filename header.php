b9da154260bc23db615ff9a1128366578110a365
edrlgo <emi.lop96@gmail.com>
2017-04-27 10:21:39 +0200

    Bootstrap + jQuery + Font Awesome

--- header.php
+++ header.php
@@ -33,7 +33,10 @@
 ================================================== -->
 
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-
+<script src="https://use.fontawesome.com/3f8e8c66c9.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
 <?php wp_head(); ?>
 
